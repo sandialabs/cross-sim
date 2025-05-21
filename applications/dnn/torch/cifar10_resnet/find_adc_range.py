a155e269fb05e11ddaf438113f065e47df0a2d26
Giorgos Kollias <114400102+aksyristos@users.noreply.github.com>
2025-05-21 16:04:42 +0200

    Update paths

--- applications/dnn/torch/cifar10_resnet/find_adc_range.py
+++ applications/dnn/torch/cifar10_resnet/find_adc_range.py
@@ -1,55 +1,163 @@
+"""
+Parameterizable inference simulation script for CIFAR-10 ResNets.
+"""
+import os
+import torch
+from torchvision import datasets, transforms
 import numpy as np
+import warnings, sys, time
+from build_resnet_cifar10 import ResNet_cifar10
+warnings.filterwarnings('ignore')
+#sys.path.append("../../") # to import dnn_inference_params
+#sys.path.append("../../../../") # to import simulator
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../"))) # to import dnn_inference_params
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../../../"))) # to import simulator
+from simulator import CrossSimParameters
+from simulator.algorithms.dnn.torch.convert import from_torch, convertible_modules, reinitialize
+from find_adc_range import find_adc_range
+from dnn_inference_params import dnn_inference_params
 
-def find_adc_range(params_args, n_layers, depth):
-    """
-    Given a set of arguments to dnn_inference_params(), find a compatible set
-    of calibrated ADC ranges. If a calibrated range does not exist, this function
-    will throw an error.
-    """
+## Depth parameter for model selection
+# Follows definition in original ResNet paper (He et al, CVPR 2016)
+# n = 2 : ResNet-14 (175K weights)
+# n = 3 : ResNet-20 (272K weights)
+# n = 5 : ResNet-32 (467K weights)
+# n = 9 : ResNet-56 (856K weights)
+n = 3
 
-    # Relevant inputs for selecting ADC and activation ranges
-    adc_range_option = params_args['adc_range_option']
-    adc_bits = params_args['adc_bits']
-    style = params_args['core_style']
-    NrowsMax = params_args['NrowsMax']
-    adc_per_ibit = (params_args['input_bitslicing'] 
-        and params_args['input_slice_size'] == 1
-        and params_args['adc_per_ibit'])
-    Nslices = params_args['Nslices']
-    subtract_current_in_xbar = (params_args['subtract_current_in_xbar'] or 
-        params_args['interleaved_posneg'])
+useGPU = False # use GPU?
+N = 10000 # number of images
+batch_size = 64
+Nruns = 1
+print_progress = True
 
-    adc_ranges = [None] * n_layers
+depth = 6*n+2
+print("Model: ResNet-{:d}".format(depth))
+print("CIFAR-10: using "+("GPU" if useGPU else "CPU"))
+print("Number of images: {:d}".format(N))
+print("Number of runs: {:d}".format(Nruns))
+print("Batch size: {:d}".format(batch_size))
+device = torch.device("cuda:0" if (torch.cuda.is_available() and useGPU) else "cpu")
 
-    # An NrowsMax value of None really means no upper bound
-    if NrowsMax is None:
-        NrowsMax = 1e6
+##### Load Pytorch model
+resnet_model = ResNet_cifar10(n)
+resnet_model = resnet_model.to(device)
+#resnet_model.load_state_dict(
+#    torch.load('./models/resnet{:d}_cifar10.pth'.format(depth),
+#    map_location=torch.device(device)))
+model_path = os.path.join(os.path.dirname(__file__), 'models','resnet{:d}_cifar10.pth'.format(depth))
+resnet_model.load_state_dict(
+      torch.load(model_path, map_location=torch.device(device)))
+resnet_model.eval()
+n_layers = len(convertible_modules(resnet_model))
 
-    # The optimal ADC range depends on several factors
-    #   - Number of weight bit slices
-    #   - The negative weight handling scheme (style)
-    #   - The array size (NrowsMax)
-    #   - Whether input bit accumulation is done in analog (relevant for offset and bit slicing)
-    #   - For balanced core, whether current is subtracted in analog (here, always assumed true)
- 
-    limits_dir = "./calibrated_config/"
+##### Set the simulation parameters
 
-    if adc_bits > 0 and adc_range_option == "CALIBRATED":
+# Create a list of CrossSimParameters objects
+params_list = [None] * n_layers
 
-        if Nslices == 1:
-            if style == "BALANCED" and NrowsMax >= 576 and not adc_per_ibit and subtract_current_in_xbar:
-                adc_ranges = np.load(limits_dir+"adc_limits_ResNet{:d}_balanced.npy".format(depth))
+# Params arguments common to all layers
+base_params_args = {
+    'ideal' : False,
+    ## Mapping style
+    'core_style' : "BALANCED",
+    'Nslices' : 1,
+    ## Weight value representation and precision
+    'weight_bits' : 8,
+    'weight_percentile' : 100,
+    'digital_bias' : True,
+    ## Memory device
+    'Rmin' : 1e4,
+    'Rmax' : 1e6,
+    'infinite_on_off_ratio' : False,
+    'error_model' : "none",
+    'alpha_error' : 0.0,
+    'proportional_error' : False,
+    'noise_model' : "none",
+    'alpha_noise' : 0.0,
+    'proportional_noise' : False,
+    'drift_model' : "none",
+    't_drift' : 0,
+    ## Array properties
+    'NrowsMax' : 1152,
+    'NcolsMax' : None,
+    'Rp_row' : 0, # ohms
+    'Rp_col' : 0, # ohms
+    'interleaved_posneg' : False,
+    'subtract_current_in_xbar' : True,
+    'current_from_input' : True,
+    ## Input quantization
+    'input_bits' : 8,
+    'input_bitslicing' : False,
+    'input_slice_size' : 1,
+    ## ADC
+    'adc_bits' : 8,
+    'adc_range_option' : "CALIBRATED",
+    'adc_type' : "generic",
+    'adc_per_ibit' : False,
+    ## Simulation parameters
+    'useGPU' : useGPU
+    }
 
-            elif style == "BALANCED" and NrowsMax >= 576 and adc_per_ibit and subtract_current_in_xbar:
-                adc_ranges = np.load(limits_dir+"adc_limits_ResNet{:d}_balanced_ibits.npy".format(depth))
+### Load input limits
+input_ranges = np.load(os.path.join(os.path.dirname(__file__),'calibrated_config/input_limits_ResNet{:d}.npy').format(depth))
 
-            elif style == "OFFSET" and NrowsMax >= 576 and adc_per_ibit:
-                adc_ranges = np.load(limits_dir+"adc_limits_ResNet{:d}_offset.npy".format(depth))
+### Load ADC limits
+adc_ranges = find_adc_range(base_params_args, n_layers, depth)
+
+### Set the parameters
+for k in range(n_layers):
+    params_args_k = base_params_args.copy()
+    params_args_k['positiveInputsOnly'] = (False if k == 0 else True)
+    params_args_k['input_range'] = input_ranges[k]
+    params_args_k['adc_range'] = adc_ranges[k]
+    params_list[k] = dnn_inference_params(**params_args_k)
+
+#### Convert PyTorch layers to analog layers
+analog_resnet = from_torch(resnet_model, params_list, fuse_batchnorm=True, bias_rows=0)
+
+#### Load and transform CIFAR-10 dataset
+normalize = transforms.Normalize(
+    mean = [0.485, 0.456, 0.406],
+    std  = [0.229, 0.224, 0.225])
+dataset = datasets.CIFAR10(root='./',train=False, download=True, 
+    transform= transforms.Compose([transforms.ToTensor(), normalize]))
+dataset = torch.utils.data.Subset(dataset, np.arange(N))
+cifar10_dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
+
+#### Run inference and evaluate accuracy
+accuracies = np.zeros(Nruns)
+for m in range(Nruns):
+
+    T1 = time.time()
+    y_pred, y, k = np.zeros(N), np.zeros(N), 0
+    for inputs, labels in cifar10_dataloader:
+        inputs = inputs.to(device)
+        output = analog_resnet(inputs)
+        output = output.to(device)
+        y_pred_k = output.data.cpu().detach().numpy()
+        if batch_size == 1:
+            y_pred[k] = y_pred_k.argmax()
+            y[k] = labels.cpu().detach().numpy()
+            k += 1
         else:
-            raise ValueError("No calibrated ADC range found for the combination of crossbar style, adc_per_ibit and # slices")
+            batch_size_k = y_pred_k.shape[0]
+            y_pred[k:(k+batch_size_k)] = y_pred_k.argmax(axis=1)
+            y[k:(k+batch_size_k)] = labels.cpu().detach().numpy()
+            k += batch_size_k
+        if print_progress:
+            print("Image {:d}/{:d}, accuracy so far = {:.2f}%".format(
+                k, N, 100*np.sum(y[:k] == y_pred[:k])/k), end="\r")
+    
+    T2 = time.time()
+    top1 = np.sum(y == y_pred)/len(y)
+    accuracies[m] = top1
+    print("\nInference finished. Elapsed time: {:.3f} sec".format(T2-T1))
+    print('Accuracy: {:.2f}% ({:d}/{:d})\n'.format(top1*100,int(top1*N),N))
+    if m < (Nruns - 1):
+        reinitialize(analog_resnet)
 
-        if adc_ranges[0] is None:
-            raise ValueError("For the chosen crossbar settings, calibrated ADC ranges are unavailable for this network: "+\
-                "please generate the limits, choose another range setting ('MAX' or 'GRANULAR'), or disable ADC.")
-
-    return adc_ranges
+if Nruns > 1:
+    print("==========")
+    print("Mean accuracy:  {:.2f}%".format(100*np.mean(accuracies)))
+    print("Stdev accuracy: {:.2f}%".format(100*np.std(accuracies)))
